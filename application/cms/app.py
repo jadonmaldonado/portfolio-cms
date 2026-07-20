153275a2b39b358c1cf200118a53ffcc2578f7d8
Jadon Maldonado <jadon.maldonado@lajollagroup.com>
2026-07-20 15:26:53 -0700

    Build initial Flask CMS application

--- application/cms/app.py
+++ application/cms/app.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template
+from routes.login import login_bp
 
 app = Flask(__name__)
+app.register_blueprint(login_bp)
 
 @app.route("/")
-def home():
+def dashboard():
     return render_template("dashboard.html")
 
 if __name__ == "__main__":
