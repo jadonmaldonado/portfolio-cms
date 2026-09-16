3ed1dcd21e0b4016f4133bafe71f322d072f4541
Jadon Maldonado <jadonmaldonado@ymail.com>
2026-09-15 18:12:39 -0700

    Add HTTPS endpoint for portfolio API

--- infrastructure/terraform/alb.tf
+++ infrastructure/terraform/alb.tf
@@ -46,8 +46,83 @@ resource "aws_lb_listener" "http" {
   protocol          = "HTTP"
 
   default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
+
+data "aws_route53_zone" "portfolio" {
+  name         = "jadonmaldonado.com"
+  private_zone = false
+}
+
+resource "aws_acm_certificate" "api" {
+  domain_name       = "api.jadonmaldonado.com"
+  validation_method = "DNS"
+
+  tags = {
+    Name = "${var.project_name}-api-cert"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_route53_record" "api_certificate_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.api.domain_validation_options :
+    dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  zone_id         = data.aws_route53_zone.portfolio.zone_id
+  name            = each.value.name
+  type            = each.value.type
+  ttl             = 60
+  records         = [each.value.record]
+  allow_overwrite = true
+}
+
+resource "aws_acm_certificate_validation" "api" {
+  certificate_arn = aws_acm_certificate.api.arn
+
+  validation_record_fqdns = [
+    for record in aws_route53_record.api_certificate_validation :
+    record.fqdn
+  ]
+}
+
+resource "aws_lb_listener" "https" {
+  load_balancer_arn = aws_lb.app.arn
+  port              = 443
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
+  certificate_arn   = aws_acm_certificate_validation.api.certificate_arn
+
+  default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.app.arn
+  }
+}
+
+resource "aws_route53_record" "api" {
+  zone_id = data.aws_route53_zone.portfolio.zone_id
+  name    = "api.jadonmaldonado.com"
+  type    = "A"
+
+  alias {
+    name                   = aws_lb.app.dns_name
+    zone_id                = aws_lb.app.zone_id
+    evaluate_target_health = true
   }
 }
 
